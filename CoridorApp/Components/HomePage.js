8cec9c994dc907abaeed9c78e7257df749de586b
berketonoz <btonoz@sabanciuniv.edu>
2022-04-18 11:20:43 +0300

    Components folder and HomePage component created

---
+++ CoridorApp/Components/HomePage.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    TouchableOpacity,
+} from 'react-native';
+
+const Separator = () => (
+    <View style={styles.separator} />
+);
+
+export default function HomePage() {
+    
+    return (
+        <SafeAreaView style={styles.container}>
+            <View>
+                <Text style={styles.title}>Welcome To HomePage</Text>
+                <Separator />
+                <TouchableOpacity style={styles.button}>
+                    <Text>Single Player</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Text>Multiplayer</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+}
+  
+const styles = StyleSheet.create({
+    container: {
+        marginTop: '10%',
+        marginLeft: '5%',
+        marginRight: '5%',
+        borderWidth: 2,
+        borderRadius: 10,
+        padding: '2%',
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    single: {
+        fontSize: 20,
+        fontWeight: '400',
+    },
+    double: {
+        fontSize: 20,
+        fontWeight: '400',
+    },
+    highlight: {
+        fontWeight: '700',
+    },
+    separator: {
+        margin: '2%',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+    },
+    button: {
+        alignItems: 'center',
+        padding: 10,
+        marginTop: '5%',
+        marginLeft:'30%',
+        marginRight:'30%',
+        borderRadius: 10,
+        backgroundColor: '#af0a00',
+    },
+});
